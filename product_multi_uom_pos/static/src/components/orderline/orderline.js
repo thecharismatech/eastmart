c531aa3dbcc83094924769750b753aaee2a4efc8
thecharismatech <zeos1999@gmail.com>
2024-11-03 11:42:52 +0000

    more

---
+++ product_multi_uom_pos/static/src/components/orderline/orderline.js
@@ -0,0 +1,23 @@
+/** @odoo-module */
+import { Orderline } from "@point_of_sale/components/orderline/orderline";
+import { useRef } from "@odoo/owl";
+import { patch } from "@web/core/utils/patch";
+
+patch(Orderline.prototype, {
+    props: {
+        ...Orderline.props,
+        line: {
+            shape: {
+                multiUom: { type: Array, optional: true },
+                resetUom: { type: Function, optional: true },
+                getUom: { type: Function, optional: true },
+                onSelectionChangedUom: { type: Function, optional: true },
+            },
+        },
+    },
+
+    setup() {
+        super.setup();
+        this.select_uom = useRef("uom_value");
+    },
+});
---
+++ product_multi_uom_pos/static/src/models/orderline.js
@@ -0,0 +1,14 @@
+/** @odoo-module */
+import { patch } from "@web/core/utils/patch";
+import { Orderline } from "@point_of_sale/store/models";
+
+patch(Orderline.prototype, {
+    export_as_JSON() {
+        const json = super.export_as_JSON();
+        if (this.product_uom_id === undefined) {
+            this.product_uom_id = this.product.uom_id;
+        }
+        json.product_uom_id = this.product_uom_id[0];
+        return json;
+    }
+});
